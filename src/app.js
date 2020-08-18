35dfc5dd8bbc411d152df6aac3fe4c35681a9e49
raphaelaugustb <travassosraphael12@gmail>
2020-08-18 10:25:24 -0300

    master

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,73 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+  const project = {
+    id:uuid(),
+    title,
+    url,
+    techs,
+    likes:0
+  }
+  repositories.push(project)
+  return response.json(project)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const { title, url, techs } = request.body
+  
+  const repositoriesIndex = repositories.findIndex(repositories => repositories.id === id)
+  if(repositoriesIndex < 0){
+   response.status(400).json({ error:"Project not Found" })
+  }   
+   
+  const project = {
+    title,
+    url,
+    techs,
+    likes:0,
+   }
+   
+   repositories[repositoriesIndex] = project
+   return response.json(project)
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const { title, url, techs } = request.body
+  const project = {
+    id:uuid(),
+    title,
+    url,
+    techs,
+    likes:0,
+  }
+  const repositoriesIndex = repositories.findIndex(repositories => repositories.id === id)
+  if(repositoriesIndex < 0){
+    return response.status(404).json({ error:"Project not Found" })
+  }else{
+  repositories.splice(repositoriesIndex,1)
+  }
+  return response.status(204).send()
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const { title, url, techs } = request.body
+  const repositoriesSearch = repositories.find(repositories => repositories.id === id)
+  if(!repositoriesSearch){
+   response.status(400).json({ error:"Project not Found" })
+  }   
+  repositoriesSearch.likes += 1
+  
+  return response.json(repositoriesSearch)
+ 
 });
 
 module.exports = app;
